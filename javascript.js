2e26e8491593ee47af46b12a7c72965d09581330
Jordan Johns <jordanrossjohns@gmail.com>
2015-09-10 11:18:37 -0500

    player one and player two now alternating

--- javascript.js
+++ javascript.js
@@ -1,22 +1,53 @@
+$(document).ready(function(){
 $(function () {
+// define playerOne and playerTwo variables:
+	var turnCount=0;
 
-	function playerOne() {
-		$(".butt").on("click",function(){
+	$(".butt").click(function(){
+		if(turnCount %2 === 0){
 			($ (this) .replaceWith ("X") );
-			
-		});
-		
-	}
-	playerTwo();	
+		}else{
+			($ (this) .replaceWith ("O") );
+		}
 
-	function playerTwo() {
-	$(".butt").on("click",function(){
-		($ (this) .replaceWith ("O") );
+		turnCount++;
+	})
+
+	// var playerOne = function playerOne() {
+	// 	$(".butt").on("click",function(){
 			
-	});
+			
+	// 	});
+	// }
 		
-	}
-	playerOne();	
+
+	// var playerTwo = function playerTwo() {
+	// 	$(".butt").on("click",function(){
+	// 		($ (this) .replaceWith ("O") );
+			
+	// 	});
+	})
+// playerOne goes first marking X
+// playerOne()	
+// determine when playerOne went last
+
+// if playerOne went last, make it playerTwo's turn
+
+// if playerTwo went last, make it playerOne's turn
+
+// alternate turns until a player has 3 rows, 3 columns, or 3 diagonals filled with their peice
+
+// determine winner
+
+//reset button		
+	$('#reset').click(function() {
+	    location.reload();
+	});
 
 
 });
+
+
+
+
+
